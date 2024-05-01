39f9ea5aa4c5818b5c72e6a8fc8c63e45563860a
Daniel <daniel.garciaortega@cieep.com>
2024-05-01 16:30:30 +0200

    Agregada configuración de entorno para producción con URL base de la API.

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+// Definición de la constante 'environment' que contiene la configuración del entorno de producción
+export const environment = {
+    // Indica si la aplicación está en modo producción (true) o desarrollo (false)
+    production: true,
+    // Define la URL base de la API a la que se realizarán las solicitudes
+    apiUrl: 'https://api.pokemontcg.io/v2'
+};
