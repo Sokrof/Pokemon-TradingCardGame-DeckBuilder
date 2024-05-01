3b7fe3e1d3a9697d21fd54c82d0ccb773f041e75
Daniel <daniel.garciaortega@cieep.com>
2024-05-01 13:31:26 +0200

    Init rama secundaria & Añadidos comentarios

--- src/app/app.routes.module.ts
+++ src/app/app.routes.module.ts
@@ -4,15 +4,18 @@ import { LoginComponent } from './components/login/login.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { RegistroComponent } from './components/registro/registro.component';
 
+// ---- RUTAS ---- //
 const routes: Routes = [
   { path: 'inicio', component: InicioComponent }, // Ruta para la página de inicio
-  { path: 'login', component: LoginComponent }, // Ruta para el LoginComponent
+  { path: 'login', component: LoginComponent }, // Ruta para el Login
   { path: '', redirectTo: '/inicio', pathMatch: 'full' }, // Ruta por defecto, redirige '/' a '/inicio'
-  { path: 'registro', component: RegistroComponent }, // Ruta para el LoginComponent
+  { path: 'registro', component: RegistroComponent }, // Ruta para el Registro
 ];
 
+// Importación del módulo RouterModule y configuración de las rutas principales
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)], // Importa y configura las rutas principales
+  exports: [RouterModule] // Exporta el módulo RouterModule para su uso en otros módulos
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { } // Clase del módulo de enrutamiento de la aplicación
+
