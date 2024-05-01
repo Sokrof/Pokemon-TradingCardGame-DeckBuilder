8d647191400821a380895beaf285b7cff09e2e7f
Daniel <daniel.garciaortega@cieep.com>
2024-05-01 18:29:18 +0200

    Añadido Servicio y modelo para Mazos

---
+++ src/app/_models/deck.model.ts
@@ -0,0 +1,10 @@
+// Importación del modelo Card para representar las cartas en una baraja
+import { Card } from "./card.model";
+
+// Definición de la clase Deck que representa una baraja de cartas
+export class Deck {
+    // Propiedades de la baraja
+    id: string = ''; // Identificador único de la baraja
+    name: string = ''; // Nombre de la baraja
+    cards: Card[] = []; // Lista de cartas que componen la baraja
+}
